da6b279116366d6e4001021f1dff06238c9f2bce
Michael Connor Buchan <mikey@blindcomputing.org>
2023-04-13 12:56:52 +0100

    Feat: Allow specifying context attributes upon construction
    We now have a ContextAttrs class and its from_kwargs() static method,
    which converts each kwarg name into its enum value with
    alcGetEnumValue(), and builds the array of attributes.

--- cyal/context.pyx
+++ cyal/context.pyx
@@ -1,18 +1,21 @@
 # cython: language_level=3
 
+from cpython cimport array
+import array
 from contextlib import contextmanager
 
 from .device cimport Device
 from .exceptions cimport raise_alc_error
-from . cimport alc
+from . cimport al, alc
 
 cdef class Context:
     cdef alc.ALCcontext* _ctx
     cdef readonly Device device
 
-    def __cinit__(self, Device dev not None):
+    def __cinit__(self, Device dev not None, **kwargs):
         self.device = dev
-        self._ctx  =alc.alcCreateContext(dev._device, NULL)
+        cdef ContextAttrs attrs = ContextAttrs.from_kwargs(dev, **kwargs)
+        self._ctx  =alc.alcCreateContext(dev._device, &attrs._attrs[0])
         if self._ctx is NULL:
             raise_alc_error(dev._device)
 
@@ -59,3 +62,31 @@ cdef class Context:
             yield self
         finally:
             alc.alcProcessContext(self._ctx)
+
+cdef array.array attrs_template = array.array('i')
+
+cdef class ContextAttrs:
+    cdef alc.ALCint[:] _attrs
+
+    def __cinit__(self, array.array attrs):
+        self._attrs = attrs
+
+    @staticmethod
+    def from_kwargs(Device dev, **kwargs):
+        cdef:
+            Py_ssize_t length = len(kwargs) * 2 + 1
+            array.array attrs_array = array.clone(attrs_template, length, zero=False)
+            alc.ALCint[:] attrs = attrs_array
+            bytes enum_name
+            alc.ALCenum enum_val
+            Py_ssize_t idx = 0
+        for k, v in kwargs.items():
+            enum_name = b"ALC_" + k.upper().encode("utf-8")
+            enum_val = alc.alcGetEnumValue(dev._device, <const alc.ALCchar *>enum_name)
+            if enum_val == al.AL_NONE:
+                raise TypeError(f"'{k} is an invalid keyword argument for ContextAttrs")
+            attrs[idx] = enum_val
+            attrs[idx+1] = <alc.ALCint>v
+            idx += 2
+        attrs[idx] = 0
+        return ContextAttrs(attrs_array)
